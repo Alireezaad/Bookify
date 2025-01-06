bbe3f6d9100994df92c533428afef8c97aa655bb
Alireezaad <alireezaad@gmail.com>
2025-01-06 18:10:38 +0330

    add a provider for UtcNow add UserErrors and ApartmentErrors. add IBookingRepository add a folder for command and commandHandlers specific to booking.

---
+++ Bookify.Domain/Booking/IBookingRepository.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Domain.Apartments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Booking
+{
+    public interface IBookingRepository
+    {
+        Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+        Task<bool> IsOverlappingAsync(Apartment apartment, DateRange duration, CancellationToken cancellationToken = default);
+
+        void Add(Booking booking);
+    }
+}
