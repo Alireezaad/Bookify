7b62b4a8e472f649278141ec5f872f92b502d783
Alireza abasi <alireezaad@gmail.com>
2025-01-13 02:28:11 +0330

    add configuration for apartments and users.

--- Bookify.Infrastracture/Configurations/BookingConfiguration.cs
+++ Bookify.Infrastracture/Configurations/BookingConfiguration.cs
@@ -1,5 +1,7 @@
-﻿using Bookify.Domain.Booking;
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Booking;
 using Bookify.Domain.Shared;
+using Bookify.Domain.Users;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
@@ -19,13 +21,43 @@ namespace Bookify.Infrastracture.Configurations
 
             builder.HasKey(booking => booking.Id);
 
+            builder.OwnsOne(booking => booking.Duration);
+
             builder.OwnsOne(booking => booking.PriceForPeriod, priceBuilder =>
             {
                 priceBuilder.Property(money => money.Currency)
                     .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
             });
 
-            builder.OwnsOne(booking => booking.Duration);
+            builder.OwnsOne(booking => booking.CleaningFee, priceBuilder =>
+            {
+                priceBuilder.Property(money => money.Currency)
+                    .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+            });
+
+            builder.OwnsOne(booking => booking.AmenitiesUpCharge, priceBuilder =>
+            {
+                priceBuilder.Property(money => money.Currency)
+                    .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+            });
+
+            builder.OwnsOne(booking => booking.TotalPrice, priceBuilder =>
+            {
+                priceBuilder.Property(money => money.Currency)
+                    .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+            });
+
+            builder.Property(booking => booking.Status)
+                .HasConversion(status => (int)status, status => (BookingStatus)status);
+
+            builder.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(booking => booking.Id);
+
+            builder.HasOne<Apartment>()
+                .WithMany()
+                .HasForeignKey(booking => booking.ApartmentId);
+
 
         }
     }
